92ee64bf5b5173b514960aec46372e1a109ccccd
JoeZhouWenxuan <chrisnotkris7@gmail.com>
2017-02-16 13:58:51 +0800

    首次提交到本地仓库

---
+++ src/main/java/cn/edu/soochow/joint_ere/App.java
@@ -0,0 +1,13 @@
+package cn.edu.soochow.joint_ere;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
